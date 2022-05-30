537fb3b703d6e62f06c660753b4710acb75ed0bd
sdr04021 <43779092+sdr04021@users.noreply.github.com>
2022-05-30 21:05:39 +0900

    videostreamTest file

---
+++ scratch/videoStream/videoStreamTest.cc
@@ -0,0 +1,89 @@
+/*****************************************************
+*
+* File:  videoStreamTest.cc
+*
+* Explanation:  This script modifies the tutorial first.cc
+*               to test the video stream application.
+*
+*****************************************************/
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/netanim-module.h"
+
+using namespace ns3;
+
+//#define NS3_LOG_ENABLE
+
+/**
+ * @brief The test cases include:
+ * 1. P2P network with 1 server and 1 client
+ * 2. P2P network with 1 server and 2 clients
+ * 3. Wireless network with 1 server and 3 mobile clients
+ * 4. Wireless network with 3 servers and 3 mobile clients
+ */
+#define CASE 1
+
+NS_LOG_COMPONENT_DEFINE ("VideoStreamTest");
+
+int
+main (int argc, char *argv[])
+{
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+  
+  Time::SetResolution (Time::NS);
+  LogComponentEnable ("VideoStreamClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("VideoStreamServerApplication", LOG_LEVEL_INFO);
+
+  if (CASE == 1)
+  {
+    NodeContainer nodes;
+    nodes.Create (2);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+    pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+    NetDeviceContainer devices;
+    devices = pointToPoint.Install (nodes);
+
+    InternetStackHelper stack;
+    stack.Install (nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase ("10.1.1.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+    VideoStreamClientHelper videoClient (interfaces.GetAddress (0), 5000);
+    ApplicationContainer clientApp = videoClient.Install (nodes.Get (1));
+    clientApp.Start (Seconds (0.5));
+    clientApp.Stop (Seconds (100.0));
+
+    VideoStreamServerHelper videoServer (5000);
+    videoServer.SetAttribute ("MaxPacketSize", UintegerValue (1400));
+    //videoServer.SetAttribute ("FrameFile", StringValue ("./scratch/videoStreamer/frameList.txt"));
+    // videoServer.SetAttribute ("FrameSize", UintegerValue (4096));
+
+    ApplicationContainer serverApp = videoServer.Install (nodes.Get (0));
+    serverApp.Start (Seconds (0.0));
+    serverApp.Stop (Seconds (100.0));
+
+    pointToPoint.EnablePcap ("videoStream", devices.Get (1), false);
+    Simulator::Run ();
+    Simulator::Destroy ();
+  }
+
+  else if (CASE == 2)
+  {
+
+  }
+
+  return 0;
+}
